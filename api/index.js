e4a97e8d9ff00b2f6d3106badfdd5dd2f6c9007f
*vivek <vrvivekofficial@gmail.com>
2024-09-12 11:51:58 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { error } from "console";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Mongodb connected successfully");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
