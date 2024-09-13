c916fd2611ec79b5099411a46a271bc3b4342baf
*vivek <vrvivekofficial@gmail.com>
2024-09-13 14:09:19 +0530

    redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,14 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App.jsx";
-import { store } from "./redux/store.js";
+import { store, persistor } from "./redux/store.js";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
 );
