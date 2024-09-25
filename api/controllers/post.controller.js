3862aaffed48ed1f9ebbeb94e7d16c45bdab8f42
*vivek <vrvivekofficial@gmail.com>
2024-09-25 12:26:54 +0530

    create getpost api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -26,3 +26,43 @@ export const create = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getPost = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.body.startIndex) || 0;
+    const limit = req.query.limit || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalPOst = await Post.countDocuments();
+
+    const now = new Date();
+    const oneMonthAgo = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate());
+
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      posts,
+      totalPOst,
+      lastMonthPosts,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyingUser.js";
-import { create } from "../controllers/post.controller.js";
+import { create, getPost } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
+router.get('/getpost',getPost)
 
 export default router;
