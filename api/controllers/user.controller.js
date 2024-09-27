f5e95c77aaf2bba309a2367f6b649205d4c7a721
*vivek <vrvivekofficial@gmail.com>
2024-09-27 13:39:52 +0530

    show the comments of the post

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -108,3 +108,16 @@ export const getUsers = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return next(errorHandler("User not found"));
+    }
+    const { password, ...rest } = user._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
