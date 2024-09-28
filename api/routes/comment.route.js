64ae6bfec7a6f3ffde946c774979f3f31f9f66ea
*vivek <vrvivekofficial@gmail.com>
2024-09-28 14:24:03 +0530

    add edit functionality to the comment component

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { createComment, getPostComments, likeComment } from "../controllers/comment.controller.js";
+import {
+  createComment,
+  editComment,
+  getPostComments,
+  likeComment,
+} from "../controllers/comment.controller.js";
 import { verifyToken } from "../utils/verifyingUser.js";
 
 const router = express.Router();
@@ -7,5 +12,6 @@ const router = express.Router();
 router.post("/create", verifyToken, createComment);
 router.get("/getPostComments/:postId", getPostComments);
 router.put("/likeComment/:commentId", verifyToken, likeComment);
+router.put("/editComment/:commentId", verifyToken, editComment);
 
 export default router;
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -82,6 +82,12 @@ const CommentSection = ({ postId }) => {
     }
   };
 
+  const handleEdit = async (comment, editedContent) => {
+    setComments(
+      comments.map((c) => (c._id === comment._id ? { ...c, content: editedContent } : c))
+    );
+  };
+
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
@@ -137,7 +143,7 @@ const CommentSection = ({ postId }) => {
             </div>
           </div>
           {comments?.map((comment) => (
-            <Comment key={comment._id} comment={comment} onLike={handleLike} />
+            <Comment key={comment._id} comment={comment} onLike={handleLike} onEdit={handleEdit} />
           ))}
         </>
       )}
