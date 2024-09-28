b7cee930f71bf03365414f99fd4d7178cd9e8355
*vivek <vrvivekofficial@gmail.com>
2024-09-28 12:22:05 +0530

    add like functionality to the comment component

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import moment from "moment";
+import { FaThumbsUp } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
-const Comment = ({ comment }) => {
+const Comment = ({ comment, onLike }) => {
+  const { currentUser } = useSelector((store) => store.user);
   const [user, setUser] = useState({});
 
   useEffect(() => {
@@ -35,7 +38,21 @@ const Comment = ({ comment }) => {
           </span>
           <span className="text-gray-500 text-xs ">{moment(comment.createdAt).fromNow()}</span>
         </div>
-        <p className="text-gray-500 pb-2">{ comment.content}</p>
+        <p className="text-gray-500 pb-2">{comment.content}</p>
+        <div className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
+          <button
+            type="button"
+            onClick={() => onLike(comment._id)}
+            className={`text-gray-400 hover:text-blue-500 ${
+              currentUser && comment.likes.includes(currentUser._id) && "!text-blue-500"
+            }`}>
+            <FaThumbsUp className="text-sm" />
+          </button>
+          <p className="text-gray-500">
+            {comment.numberOfLikes > 0 &&
+              comment.numberOfLikes + " " + (comment.numberOfLikes === 1 ? "like" : "likes")}
+          </p>
+        </div>
       </div>
     </div>
   );
