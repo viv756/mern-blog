beb33eba31d38c584732b765554d7c00252caa10
*vivek <vrvivekofficial@gmail.com>
2024-09-30 16:29:00 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -7,6 +7,8 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 
+import path from "path";
+
 dotenv.config();
 
 mongoose
@@ -17,6 +19,8 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -31,6 +35,11 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
